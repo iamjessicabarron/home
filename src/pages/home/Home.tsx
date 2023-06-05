2cc1265eefb68eb9a2435e532c8a24683b14542d
Jessica Barron <iamjessicabarron@gmail.com>
2023-06-05 11:46:49 +0200

    cleanup project files for initial dev

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function Home() {
+    return (<div className="mainContainer">
+        <div className="mainContent">
+            <h1>
+                Hi, I'm Jess 👋🏻
+            </h1>
+            <p>I'm a Front End and aspiring Full Stack Developer from Brisbane, Australia, with heavy experience in ReactJS + TypeScript.</p>
+        </div>
+    </div>)
+}
+
+export default Home;
