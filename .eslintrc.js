48f91ead796951a5e02d3d6f02fdd75a88781c39
Jessica Barron <iamjessicabarron@gmail.com>
2023-06-05 12:58:36 +0200

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,15 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['standard', 'plugin:react/recommended'],
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: ['tsconfig.json']
   },
   plugins: ['react'],
-  rules: {}
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/space-before-function-paren': 0
+  }
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,15 @@
-import './Home.css';
+import React from 'react'
+import './Home.css'
 
 function Home() {
-    return (<div className="mainContainer">
-        <div className="mainContent">
-            <h1>
-                Hi, I'm Jess 👋🏻
-            </h1>
-            <p>I'm a Front End and aspiring Full Stack Developer from Brisbane, Australia, with heavy experience in ReactJS + TypeScript.</p>
-        </div>
-    </div>)
+  return (<div className="mainContainer">
+    <div className="mainContent">
+      <h1>
+        {'Hi, I\'m Jess 👋🏻'}
+      </h1>
+      <p>{'I\'m a Front End and aspiring Full Stack Developer from Brisbane, Australia, with heavy experience in ReactJS + TypeScript.'}</p>
+    </div>
+  </div>)
 }
 
-export default Home;
+export default Home
